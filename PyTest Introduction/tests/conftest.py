1bc61a2c3f0b47d953db6bc25eb7bcef035b807a
natshabat <teximnat@gmail.com>
2026-04-10 14:57:18 +0300

    PyTest_Homework-1004-2026

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -2,9 +2,25 @@ import pytest
 import pandas as pd
 
 # Fixture to read the CSV file
-
+@pytest.fixture(scope="session")
+def read_csv_file():
+    def _read(file_path):
+        return pd.read_csv(file_path)
+    return _read
 
 # Fixture to validate the schema of the file
+@pytest.fixture(scope="session")
+def validate_schema():
+    def _validate(actual_schema, expected_schema):
+        return list(actual_schema)==list(expected_schema)
+    return _validate
 
 
 # Pytest hook to mark unmarked tests with a custom mark
+def pytest_collection_modifyitems(items):
+    for item in items:
+        custom_marks = [mark.name for mark in item.iter_markers()]
+
+        if not custom_marks:
+            item.add_marker(pytest.mark.unmarked)
+
--- PyTest Introduction/tests/test_csv/test_csv_file.py
+++ PyTest Introduction/tests/test_csv/test_csv_file.py
@@ -1,30 +1,49 @@
+import pandas as pd
 import pytest
 import re
 
+file_path=r"C:\\Users\\NataliyaShabat\\dqe-automation\\PyTest Introduction\\src\\data\\data.csv"
 
-def test_file_not_empty():
-    assert 1 + 1 == 2
+def test_file_not_empty(read_csv_file):
+    df =read_csv_file(file_path)
+    assert not df.empty, "data.csv is empty"
+
+@pytest.mark.validate_csv
+def test_validate_schema(read_csv_file,validate_schema):
+    df =read_csv_file(file_path)
+    expected_colunms = ["id","name","age","email","is_active"]
+    assert validate_schema(df.columns, expected_colunms), f"Schema mismatch.Expected {expected_colunms}, got {list(df.columns)}"
+
+@pytest.mark.validate_csv
+@pytest.mark.skip 
+def test_age_column_valid(read_csv_file):
+    df =read_csv_file(file_path)
+    assert df["age"].between(0,100).all, "Invalid values in AGE column"
+    
+@pytest.mark.validate_csv
+def test_email_column_valid(read_csv_file):
+    df =read_csv_file(file_path)
+    pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+    invalid_emails = df[~df["email"].str.match(pattern)]
+    assert invalid_emails.empty, f"Invalid email: {invalid_emails["email"].tolist()}"
+
+@pytest.mark.validate_csv
+@pytest.mark.xfail (reason="Known duplicate")
+def test_duplicates(read_csv_file):
+    df =read_csv_file(file_path)
+    duplicates = df[df.duplicated()]
+    assert duplicates.empty, f"Duplicates found:{duplicates}"
+
+@pytest.mark.parametrize("id, is_active", [(1,False),(2,True)])
+def test_active_players(id, is_active, read_csv_file):
+    df =read_csv_file(file_path)
+    actual_value =df.loc[df["id"]==id,"is_active"].iloc[0]
+    assert actual_value == is_active, f"For id {id}:expected is_active={is_active}, but got{actual_value}"
 
 
-def test_duplicates():
-    assert 1 + 1 == 2
-
-
-def test_validate_schema():
-    assert 1 + 1 == 2
-
-
-def test_age_column_valid():
-    assert 1 + 1 == 2
-
-
-def test_email_column_valid():
-    assert 1 + 1 == 2
-
-
-def test_active_players():
-    assert 1 + 1 == 2
-
-
-def test_active_player():
-    assert 1 + 1 == 2
+def test_active_player(read_csv_file):
+    df =read_csv_file(file_path)
+    actual_values = df.loc[df["id"] == 2, "is_active"].values
+    assert len(actual_values) > 0, "No record found for ID 2"
+    actual_value = actual_values[0]
+    assert actual_value == True, f"Expected is_active=True for ID 2, but got {actual_value}"
